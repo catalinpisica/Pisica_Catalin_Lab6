41e2851e79aa4fc3d1377b22a3176937318e2747
catalinpisica5 <catalinpisica5@gmail.com>
2021-01-08 16:49:49 +0200

    Add project files.

---
+++ Data/Pisica_Catalin_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pisica_Catalin_Lab6.Models;
+
+namespace Pisica_Catalin_Lab6.Data
+{
+    public class Pisica_Catalin_Lab6Context : DbContext
+    {
+        public Pisica_Catalin_Lab6Context (DbContextOptions<Pisica_Catalin_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pisica_Catalin_Lab6.Models.Book> Book { get; set; }
+    }
+}
